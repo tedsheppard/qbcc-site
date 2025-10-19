6c3b529653ebd92d7e80e57610bb1e7ec4358617
tedsheppard <edwardsheppard5@gmail.com>
2025-10-19 12:27:45 +0000

    Temporarily force all queries to use SQLite FTS for testing purposes

--- server.py
+++ server.py
@@ -995,7 +995,7 @@ def search_fast(
     )
     
     # --- BRANCH 1: SQLite FTS for Advanced/Complex Queries ---
-    if has_advanced_filters or is_complex_query:
+    if True: # Temporarily force all queries to use SQLite FTS
         try:
             nq2 = preprocess_sqlite_query(q_norm) if q_norm else ""
 
